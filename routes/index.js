d437485f825278ad212929eeb6810a039f54a07b
juan-2401 <juan-2401@hotmail.com>
2021-12-24 15:15:34 -0300

    send

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Formulario de Contacto */
+
+router.post('/llegada-form-contacto', (req, res) => {
+  const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const body = req.body;
+
+  console.log(body);
+
+  const msg = {
+    to: 'no.reply.240191@gmail.com', // Change to your recipient
+    from: 'no.reply.240191@gmail.com', // Change to your verified sender
+    subject: body.data.nombre,
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode)
+      console.log(response[0].headers)
+      res.send(response[0].statusCode);
+    })
+    .catch((error) => {
+      console.error(error)
+      res.send(error);
+    })
+});
+
 module.exports = router;
