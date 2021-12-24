19b9f7d180a8377206c37642dbaaca61f93fb3ad
juan-2401 <juan-2401@hotmail.com>
2021-12-24 15:57:11 -0300

    send

--- routes/index.js
+++ routes/index.js
@@ -36,4 +36,38 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     })
 });
 
+/* Formulario de Dar en Adopción */
+
+router.post('/llegada-form-dar-adopcion', (req, res) => {
+  const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const body = req.body;
+
+  console.log(body);
+
+  const msg = {
+    to: 'no.reply.240191@gmail.com', // Change to your recipient
+    from: 'no.reply.240191@gmail.com', // Change to your verified sender
+    subject: `${body.data.nombre} te ha enviado un mail`,
+    text: `${body.data.nombre} quiere dar un ${body.data.animales} en adopción. 
+    Zona: ${body.data.zona}.
+    Edad: ${body.data.años} años y ${body.data.meses} meses.
+    Comentario: ${body.data.mensaje}.
+    Contactalo a su teléfono ${body.data.telefono} o a su email ${body.data.email}.`,
+  }
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode)
+      console.log(response[0].headers)
+      res.send(response[0].statusCode);
+    })
+    .catch((error) => {
+      console.error(error)
+      res.send(error);
+    })
+});
+
 module.exports = router;
